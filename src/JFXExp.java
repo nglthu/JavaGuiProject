050c7716a2c25ded1efda835f165a57541ed78b5
Lethu Nguyen <lethunguyen@Lethus-MacBook-Pro.local>
2024-03-13 16:05:00 +0700

    test

--- src/JFXExp.java
+++ src/JFXExp.java
@@ -1,15 +1,12 @@
-
-
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
  
-public class App extends Application {
+public class JFXExp extends Application {
     
    
-   /*  @Override
+    @Override
     public void start(Stage primaryStage) {
         try {
             BorderPane root = new BorderPane();
@@ -19,7 +16,7 @@ public class App extends Application {
             primaryStage.show();
         } catch(Exception e) {
             e.printStackTrace();
-        } */
+        } 
     }
      
     public static void main(String[] args) {
